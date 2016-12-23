f47789f7dd13e83b1188353d43643414276e58dd
ponpon <ponpon@i-link.tw>
2016-12-23 15:39:47 +0800

    add options for FormattedRelative

--- src/components/Show.js
+++ src/components/Show.js
@@ -5,18 +5,31 @@ import { FormattedMessage, FormattedDate, FormattedTime, FormattedRelative } fro
 class Show extends React.Component {
 	constructor() {
         super();
+
         this.currentDate = new Date();
         this.targetDate = new Date(
         	this.currentDate.getFullYear(),
         	this.currentDate.getMonth(),
         	this.currentDate.getDate(),
         	this.currentDate.getHours(),
-        	this.currentDate.getMinutes(),
-        	this.currentDate.getSeconds() + 30
+        	this.currentDate.getMinutes() + 2,
+        	this.currentDate.getSeconds()
         );
     }
 
 	render() {
+
+        // https://github.com/yahoo/react-intl/wiki/Components#formattedrelative
+		const options_FormattedRelative1 = {
+			style: 'numeric', // 看不出來設numeric跟best fit有什麼差別
+			units: 'second'
+		};
+
+		const options_FormattedRelative2 = {
+			style: 'best fit', // 看不出來設numeric跟best fit有什麼差別
+			units: 'second'
+		};
+
 		return (
 			<div>
 				<div>
@@ -38,7 +51,27 @@ class Show extends React.Component {
 					<FormattedTime value={this.currentDate}/>
 				</div>
 				<div>
-					<FormattedRelative value={this.targetDate} initialNow={this.currentDate} updateInterval={1000}/>
+					<FormattedRelative
+						value={this.targetDate}
+						initialNow={this.currentDate}
+						updateInterval={1000}
+					/>
+				</div>
+				<div>
+					<FormattedRelative
+						value={this.targetDate}
+						initialNow={this.currentDate}
+						updateInterval={1000}
+						{...options_FormattedRelative1}
+					/>
+				</div>
+				<div>
+					<FormattedRelative
+						value={this.targetDate}
+						initialNow={this.currentDate}
+						updateInterval={1000}
+						{...options_FormattedRelative2}
+					/>
 				</div>
 			</div>
 		)
