a910b9b5ce8f72ba942169fea49885f20373d92a
ponpon <ponpon@i-link.tw>
2016-12-23 16:07:20 +0800

    remove useless comment

--- src/main.js
+++ src/main.js
@@ -7,28 +7,6 @@ import Panel from './containers/Panel';
 
 import configureStore from './store/configureStore';
 
-// i18n
-// import zh_TW from './locale/zh_TW';
-// import en_US from './locale/en_US';
-// import intl from 'intl';
-// import zh from 'react-intl/locale-data/zh';
-// import en from 'react-intl/locale-data/en';
-// import {IntlProvider, addLocaleData} from 'react-intl';
-// addLocaleData([...en,...zh]);
-
-// const localeList = {
-// 	en_US: 'en',
-// 	zh_TW: 'zh'
-// };
-
-// const messagesList = {
-// 	en_US,
-// 	zh_TW
-// };
-
-// <IntlProvider locale={localeList[]} messages={zh_TW}>
-// </IntlProvider>
-
 ReactDOM.render(
 	<Provider store={configureStore}>		
 		<Panel />
